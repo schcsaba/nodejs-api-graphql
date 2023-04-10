3b92715d463106f4d95cdb696b685d6344f5d078
Csaba Schnitchen <schcsaba@gmail.com>
2023-04-10 15:28:20 +0200

    first commit

---
+++ api/controllers/auth.js
@@ -0,0 +1,120 @@
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
+exports.signup = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed.');
+        error.statusCode = 422;
+        error.data = errors.array();
+        throw error;
+    }
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+    try {
+        const hashedPw = await bcrypt.hash(password, 12);
+        const user = new User({
+            email: email,
+            password: hashedPw,
+            name: name
+        });
+        const result = await user.save();
+        res.status(201).json({ message: 'User created!', userId: result._id })
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
+
+exports.login = async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    try {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            const error = new Error('A user with this email could not be found.');
+            error.statusCode = 401;
+            throw error;
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual) {
+            const error = new Error('Wrong password!');
+            error.statusCode = 401;
+            throw error;
+        }
+        const token = jwt.sign(
+            {
+                email: user.email,
+                userId: user._id.toString()
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+        res.status(200).json({ token: token, userId: user._id.toString() });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
+
+exports.getUserStatus = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.userId)
+        if (!user) {
+            const error = new Error('User not found.');
+            error.statusCode = 404;
+            throw error;
+        }
+        if (user._id.toString() !== req.userId) {
+            const error = new Error('Not authorized!');
+            error.statusCode = 403;
+            throw error;
+        }
+        res.status(200).json({ message: 'User status fetched.', status: user.status });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
+
+exports.updateUserStatus = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed, entered data is incorrect.');
+        error.statusCode = 422;
+        throw error;
+    }
+    const status = req.body.status;
+    try {
+        const user = await User.findById(req.userId);
+
+        if (!user) {
+            const error = new Error('Could not find user.');
+            error.statusCode = 404;
+            throw error;
+        }
+        if (user._id.toString() !== req.userId) {
+            const error = new Error('Not authorized!');
+            error.statusCode = 403;
+            throw error;
+        }
+        user.status = status;
+        const result = await user.save();
+        res.status(200).json({ message: 'User status updated!', status: result.status });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
